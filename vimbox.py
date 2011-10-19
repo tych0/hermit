3387f18be18ad52ae3f52fa37bdd32cdbbc7e03d
Tycho Andersen <tycho@cs.wisc.edu>
2011-10-19 12:00:09 -0500

    Implemented a version of insert mode and escape mode.

--- vimbox.py
+++ vimbox.py
@@ -19,32 +19,77 @@ class _Textbox(Textbox):
 class VimBox(object):
   def __init__(self, win, updater, inputer, blocktime=500):
     self.win = win
-    self.win.timeout(blocktime)
-    self.textbox = _Textbox(self.win, insert_mode=True)
+    (y, x) = self.win.getmaxyx()
+    self.textwin = self.win.derwin(y-1, x, 0, 0)
+    self.textwin.timeout(blocktime)
+    self.cmdwin = self.win.derwin(1, x, y-1, 0)
+
+    self.win.leaveok(False)
+    self.textwin.leaveok(False)
+    self.cmdwin.leaveok(False)
+
+    self.textbox = _Textbox(self.textwin, insert_mode=True)
     self.textbox.stripspaces = True
 
     self.updater = updater
     self.inputer = inputer
+    self.insertmode = True
+
+    self.update()
+
+  def update(self, updater=False):
+    (cursory, cursorx) = self.win.getyx()
+
+    if updater:
+      self.updater()
+
+    if self.insertmode:
+      self.cmdwin.clear()
+      self.cmdwin.addstr(0, 0, "-- INSERT --")
+
+    self.win.move(cursory, cursorx)
+    self.win.cursyncup()
+    self.win.refresh()
 
   def __call__(self):
     def callback(ch):
-      (cursory, cursorx) = self.win.getyx()
-      self.updater()
-      self.win.move(cursory, cursorx)
-      self.win.cursyncup()
-      self.win.refresh()
-
+      self.update()
+      if ch == curses.ascii.ESC:
+        self.escmode()
+        return None
       if ch < 0:
+        self.update(updater=True)
         return None
       return ch
 
     while True:
       inp = self.textbox.edit(callback)
+      self.textwin.clear()
       inp = string.replace(inp, '\n', '')
       if inp.strip() == '/quit':
         return
-      self.win.clear()
       self.inputer(inp)
+
+  def escmode(self):
+    (cursory, cursorx) = self.win.getyx()
+
+    self.insertmode = False
+    self.cmdwin.clear()
+    self.update()
+
+    while True:
+      ch = self.cmdwin.getch()
+      if ch == 'i':
+        break
+      self.inputer('Got: ' + chr(ch))
+      self.update()
+    
+    self.insertmode = True
+    self.update()
+
+    self.win.move(cursory, cursorx)
+    self.win.cursyncup()
+    self.win.refresh()
 
 if __name__ == '__main__':
   from conversation import Conversation
